1bb927ac1fac893163ef86fbb9d8395f6b9dbd26
Jun Hyeon KIM <junhyeon47@gmail.com>
2017-03-20 14:29:31 +0900

    Express Sample app: get, post sample using form tag

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 // jade를 사용하기 위한 환경 설정 시작
@@ -8,7 +9,27 @@ app.set('views', './views'); //두번째 인자는 폴더위치를 가리킨다(
 // jade를 사용하기 위한 환경 설정 끝
 
 app.use(express.static('public')); //정적인 파일이 위치할 디렉토리를 설정.
+app.use(bodyParser.urlencoded({ extended: false })); //body-parser를 사용하기 위한 작업.
 
+// form 예제를 위한 라우터 작성
+app.get('/form', function(req, res){
+  res.render('form');
+});
+
+// form 으로 넘겨진 데이터를 받기위한 get 방식 라우터 작성
+app.get('/form_receiver', function(req, res){
+  var title = req.query.title;
+  var description = req.query.description;
+  res.send(title+','+description);
+});
+
+// form 으로 넘겨진 데이터를 받기위한 post 방식 라우터 작성
+app.post('/form_receiver', function(req, res){
+  //body-parser라는 미들웨어를 사용하여 post 방식으로 전송한 데이터를 받는다.
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send(title+','+description);
+});
 
 // 쿼리 스트링을 사용하기 위한 라우터 작성(시맨틱 URL 적용)
 app.get('/topic/:id', function(req, res){
@@ -19,9 +40,9 @@ app.get('/topic/:id', function(req, res){
     'express is ...'
   ];
   var output = `
-    <a href='/topic?id=0'>javascript</a><br>
-    <a href='/topic?id=1'>node.js</a><br>
-    <a href='/topic?id=2'>express</a><br><br>
+    <a href='/topic/0'>javascript</a><br>
+    <a href='/topic/1'>node.js</a><br>
+    <a href='/topic/2'>express</a><br><br>
     ${topics[req.params.id]}
   `;
   res.send(output);
@@ -45,7 +66,7 @@ app.get('/', function (req, res) {
 app.get('/dynamic', function(req, res){
   var lis = '';
   for(var i=0; i<5; ++i){
-    lis += '<li>coding</li>'
+    lis += '<li>coding</li>';
   }
   var time = Date();
   //``: 코멘티드 텍스트
@@ -68,7 +89,7 @@ app.get('/dynamic', function(req, res){
 });
 
 app.get('/route', function(req, res){
-  res.send('Hello Router, <img src="/app_logo.png">')
+  res.send('Hello Router, <img src="/app_logo.png">');
 });
 
 app.get('/login', function (req, res) {
