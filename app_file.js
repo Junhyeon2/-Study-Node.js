c6f383f29b3a36328e42fc9b5343b7374026b24b
Jun Hyeon KIM <junhyeon47@gmail.com>
2017-03-21 12:54:15 +0900

    Express Sample app: File upload

--- app_file.js
+++ app_file.js
@@ -1,6 +1,16 @@
 var express = require('express'); //express 모듈을 획득.
 var bodyParser = require('body-parser'); //body-parser 모듈 획득.
 var fs = require('fs'); //file system 모듈 획득.
+var multer = require('multer'); //multer 모듈 획득.
+var storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb){
+    cb(null, file.originalname);
+  }
+}); //upload 미들웨어의 옵션을 사용하기 위한 객체 생성.
+var upload = multer({storage: storage}); //upload 미들웨어 획득(업로드 파일 위치 설정).
 var app = express(); //application 객체 획득.
 
 app.locals.pretty = true; // 페이지 소스보기에서 html 소스코드를 보기 쉽게 하기 위해서
@@ -8,6 +18,18 @@ app.set('views', './views_file'); //템플릿의 위치를 설정.
 app.set('view engine', 'jade'); //jade 엔진을 사용.
 
 app.use(bodyParser.urlencoded({ extended: false })); //body-parser를 사용하기 위한 작업.
+app.use('/user', express.static('uploads'));
+// 업로드 폼을 보여주기 위한 라우터 작성.
+app.get('/upload', function(req, res){
+  res.render('upload');
+});
+
+// 업로드 파일을 저장하기 위한 라우터 작성.
+// upload.single(): request 객체안에 file을 삽입해주는 미들웨어. 매개변수는 form태그의 name
+app.post('/upload', upload.single('userfile'), function(req, res){
+  console.log(req.file);
+  res.send('uploaded, '+req.file.filename);
+});
 
 app.get('/topic/new', function(req, res){
   //data폴더에서 데이터릴 읽어 온다.
